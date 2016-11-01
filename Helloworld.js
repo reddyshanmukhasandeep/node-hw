325ffa7015ea33f13d3f9e7fbe49c9983ea15b81
reddyshanmukhasandeep <reddyshanmukha.sandeep@cognizant.com>
2016-11-01 16:11:25 +0530

    Update Helloworld.js
    env variable changing

--- Helloworld.js
+++ Helloworld.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 
-var port = process.env.VCAP_APP_PORT||8080;
+var port = process.env.PORT||8080;
 http.createServer(function (request, response) {
 
       response.writeHead(200, {'Content-Type': 'text/plain'});
